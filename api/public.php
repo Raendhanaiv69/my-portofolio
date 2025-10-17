2ea34c886c44e8c8f8d05eb9bc1e070d58b4e27c
Raendhana Ilmi Vadhiasih <raendhanaiv@student.telkomuniversity.ac.id>
2025-10-17 19:41:13 +0700

    tes

---
+++ api/public.php
@@ -0,0 +1,4 @@
+<?php
+// Redirect all requests to the main public index.php file for Vercel deployment
+
+require __DIR__ . '/index.php';
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/invoice', function () {
+    return view('invoice');
+});
+Route::get('/invoice2', function () {
+    return view('invoice2');
+});
+
